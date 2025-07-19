63e84f59e0c5a777c01715acc06c436ffa4beba2
saddwarf <laidegor@gmail.com>
2025-07-19 17:01:59 +0300

    feat(assignment): initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Build
+```bash
+./build.sh
+```
+
+### Run
+```bash
+./build/defy_test_assignment
+```
---
+++ main.cpp
@@ -0,0 +1,220 @@
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <sched.h>
+#include <thread>
+#include <vector>
+
+constexpr size_t kCacheLineLen = 64;
+
+void set_affinity(const int num)
+{
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(num, &cpuset);
+    if (sched_setaffinity(0, sizeof(cpuset), &cpuset) != 0)
+    {
+        perror("sched_setaffinity");
+        exit(1);
+    }
+}
+
+class SyncState
+{
+public:
+    enum State : uint8_t
+    {
+        Empty = 0,
+        ReaderReady = 1,
+        MessageSent = 2,
+    };
+
+    SyncState() { flag.store(Empty); };
+
+    void spin_exchange(const State from, const State to)
+    {
+        while (true)
+        {
+            State tmp = from;
+            if (flag.compare_exchange_strong(tmp, to, std::memory_order_acquire))
+            {
+                break;
+            }
+            while (flag.load(std::memory_order_relaxed) != from)
+            {
+            }
+        }
+    }
+
+    void wait(const State state) const
+    {
+        while (flag.load(std::memory_order_relaxed) != state)
+        {
+        }
+    }
+
+    std::atomic<State> flag;
+};
+
+struct alignas(kCacheLineLen) SingleCacheLineMessage
+{
+    char data[kCacheLineLen];
+};
+
+struct alignas(kCacheLineLen) DoubleCacheLineMessage
+{
+    char data[2 * kCacheLineLen];
+};
+
+template <typename T>
+struct SharedState
+{
+    T msg;
+    SyncState sync;
+};
+
+template <typename T>
+T generate_message()
+{
+    T msg{};
+    for (char& byte : msg.data)
+    {
+        byte = static_cast<char>(std::rand() % 256);
+    }
+    return msg;
+}
+
+template <typename T>
+void writer(const std::shared_ptr<SharedState<T>> state, const int iterations, const int cpu_id,
+            std::vector<timespec>& times)
+{
+    timespec time;
+    set_affinity(cpu_id);
+    for (size_t i = 0; i < iterations; ++i)
+    {
+        T msg = generate_message<T>();
+        state->sync.wait(SyncState::State::ReaderReady);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+        std::memcpy(state->msg.data, &msg.data, sizeof(msg.data));
+        state->sync.spin_exchange(SyncState::State::ReaderReady, SyncState::State::MessageSent);
+        times[i] = time;
+    }
+}
+
+// Used by reader to avoid situations when read operations are optimized out by the compiler.
+DoubleCacheLineMessage out;
+
+template <typename T>
+void reader(const std::shared_ptr<SharedState<T>> state, int iterations, int cpu_id, std::vector<timespec>& times)
+{
+    timespec time;
+    set_affinity(cpu_id);
+    // MessageSent initial state used as optimization technique to avoid comparing with Empty in loop.
+    state->sync.spin_exchange(SyncState::State::Empty, SyncState::State::MessageSent);
+    for (size_t i = 0; i < iterations; ++i)
+    {
+        state->sync.spin_exchange(SyncState::State::MessageSent, SyncState::State::ReaderReady);
+        state->sync.wait(SyncState::State::MessageSent);
+        std::memcpy(out.data, &state->msg.data, sizeof(state->msg.data));
+        clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+
+        times[i] = time;
+    }
+}
+
+size_t duration_between_ns(const timespec& start, const timespec& end)
+{
+    return (end.tv_sec - start.tv_sec) * 1'000'000'000ULL + (end.tv_nsec - start.tv_nsec);
+}
+
+struct BenchmarkResult
+{
+    size_t median;
+    size_t p90;
+    size_t p95;
+};
+
+void print_results(const std::vector<std::vector<BenchmarkResult>>& results)
+{
+    std::cout << "Benchmark results (median/p90/p95) in nanoseconds:\n";
+    for (size_t i = 0; i < results.size(); ++i)
+    {
+        for (size_t j = 0; j < results[i].size(); ++j)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            std::cout << std::format("cpu{} to cpu{} = {}/{}/{}\n", i, j, results[i][j].median, results[i][j].p90,
+                                     results[i][j].p95);
+        }
+    }
+}
+
+BenchmarkResult calculate_timings(const std::vector<timespec>& send_times, const std::vector<timespec>& receive_times,
+                                  size_t skip_first_n)
+{
+    std::vector<uint64_t> durations(send_times.size() - skip_first_n);
+    for (size_t i = 0; i < durations.size(); ++i)
+    {
+        durations[i] = duration_between_ns(send_times[i + skip_first_n], receive_times[i + skip_first_n]);
+    }
+    std::sort(durations.begin(), durations.end());
+    return BenchmarkResult{.median = durations[durations.size() / 2],
+                           .p90 = durations[durations.size() * 90 / 100],
+                           .p95 = durations[durations.size() * 95 / 100]};
+}
+
+template <typename T>
+void benchmark(size_t num_threads, size_t iterations, size_t skip_first_n)
+{
+    std::vector<timespec> send_times(iterations);
+    std::vector<timespec> receive_times(iterations);
+
+    std::vector results(num_threads, std::vector<BenchmarkResult>(num_threads));
+
+    for (size_t reader_cpu = 0; reader_cpu < num_threads; ++reader_cpu)
+    {
+        for (size_t writer_cpu = 0; writer_cpu < num_threads; ++writer_cpu)
+        {
+            if (reader_cpu == writer_cpu)
+            {
+                continue;
+            }
+            std::shared_ptr<SharedState<T>> state = std::make_shared<SharedState<T>>();
+
+            std::thread t1(writer<T>, state, iterations, reader_cpu, std::ref(send_times));
+            std::thread t2(reader<T>, state, iterations, writer_cpu, std::ref(receive_times));
+
+            t1.join();
+            t2.join();
+
+
+            results[writer_cpu][reader_cpu] = calculate_timings(send_times, receive_times, skip_first_n);
+        }
+    }
+
+    print_results(results);
+}
+
+int main()
+{
+    constexpr size_t iterations = 1000;
+    constexpr size_t skip_first_n = 100;
+
+    // Generate seed for rand().
+    // Used only for generating message data. Problems with randomness doesn't really matter here.
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    size_t num_threads = std::thread::hardware_concurrency();
+
+    std::cout << "Running benchmark sending single cache line message between CPUs" << std::endl;
+    benchmark<SingleCacheLineMessage>(num_threads, iterations, skip_first_n);
+
+    std::cout << "Running benchmark sending double cache line message between CPUs" << std::endl;
+    benchmark<DoubleCacheLineMessage>(num_threads, iterations, skip_first_n);
+
+
+    return 0;
+}
